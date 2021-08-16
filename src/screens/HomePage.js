fbf03708e0da838db032c3b262f3621a14fd7326
Heranca <heranca.kamalo@gmail.com>
2021-08-16 17:08:27 +0200

    change header color & main bg color

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -25,38 +25,52 @@ const useStyles = makeStyles({
       justifyContent: 'center',
     },
   },
+  firstPart: {
+    marginTop: '14vh',
+    border: '2px solid blue',
+    width: '100vw',
+    height: '80vh',
+  },
   homeImg: {
+    border: '2px solid purple',
     margin: 0,
     overflow: 'hidden',
+    width: 1920*0.75,
+    height: 1259*2,
+    position: 'relative',
   },
   text: {
-    position: 'absolute',
-    left: 20,
-    top: 10 * 15,
-    width: '30em',
-    height: '20em',
-    color: 'white',
+    border: '1px solid red',
+    color: '#2A0800',
+    width: '30vw',
+    height: '30vh'
+  },
+  image: {
+   
+  },
+  secondPart: {
+    border: '1px solid red',
+    width: '100vw',
+    height: '80vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-end',
   },
   explicationConcept: {
+    border: '1px solid black',
     width: '100vw',
+    height: '14vh',
     display: 'flex',
     justifyContent: 'center',
+    alignItems: 'center',
+    background: '#fff',
   },
   text2: {
     margin: 10,
-    width: '70vw',
-    height: '20em',
+    border: '1px solid green',
+    width: '70%',
     textAlign: 'center',
-  },
-  image: {
-    width: 1920 * 0.8,
-    height: 1259 * 0.8,
-  },
-  secondPart: {
-    background: '#fff',
-    width: '60em',
-    display: 'flex',
-    flexDirection: 'column',
+    color: 'black',
   },
 });
 
@@ -66,26 +80,28 @@ export default function HomePage() {
   return (
     <>
       <div className={classes.root}>
-        <div className={classes.homeImg}>
-          <img className={classes.image} src={homeImage} alt="dress-home" />
+        <div className={classes.firstPart}>
+          <div className={classes.homeImg}>
+            <img className={classes.image} src={homeImage} alt="dress-home" />
+          </div>
+          <div className={classes.text}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In a hic
+            laboriosam incidunt quibusdam cupiditate. Culpa facilis, corrupti
+            distinctio fuga ratione assumenda iste maiores, explicabo in,
+            eligendi minima repellendus iusto!
+          </div>
         </div>
-        <div className={classes.text}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. In a hic
-          laboriosam incidunt quibusdam cupiditate. Culpa facilis, corrupti
-          distinctio fuga ratione assumenda iste maiores, explicabo in, eligendi
-          minima repellendus iusto!
+        <div className={classes.explicationConcept}>
+          <p className={classes.text2}>
+            {' '}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In a hic
+            laboriosam incidunt quibusdam cupiditate. Culpa facilis, corrupti
+            distinctio fuga ratione assumenda iste maiores, explicabo in,
+            eligendi minima repellendus iusto!
+          </p>
         </div>
         <div className={classes.secondPart}>
           <CategoriesSelect />
-          <div className={classes.explicationConcept}>
-            <p className={classes.text2}>
-              {' '}
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. In a hic
-              laboriosam incidunt quibusdam cupiditate. Culpa facilis, corrupti
-              distinctio fuga ratione assumenda iste maiores, explicabo in,
-              eligendi minima repellendus iusto!
-            </p>
-          </div>
         </div>
       </div>
     </>
