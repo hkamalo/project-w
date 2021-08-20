ee6098d8c215ec6f05e311e0e6f77449a0f19575
Heranca <heranca.kamalo@gmail.com>
2021-08-20 16:46:38 +0200

    add backgroung image & selecter

--- src/components/General/StaticMenu.js
+++ src/components/General/StaticMenu.js
@@ -39,6 +39,18 @@ const useStyles = makeStyles({
       padding: 0,
     },
   },
+  links: {
+    zIndex: 999,
+    border: '1px solid #87CEFA',
+    position: 'absolute',
+    background: '#fff',
+    margin: 0,
+    width: '100vw',
+    height: '15vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
 
 glide({
@@ -53,20 +65,22 @@ export default function StaticMenu(props) {
   return (
     <>
       <div className={classes.menu}>
-        <MuiThemeProvider theme={theme}>
-          <Link exact to="/" transition="glide-left">
-            <Button>Accueil</Button>
-          </Link>
-          <Link exact to="/surmesure" transition="glide-left">
-            <Button>Sur-Mesure</Button>
-          </Link>
-          <Link exact to="/tarifs" transition="glide-left">
-            <Button>Délais & Tarifs</Button>
-          </Link>
-          <Link exact to="/rendezvous" transition="glide-left">
-            <Button>Rendez-vous</Button>
-          </Link>
-        </MuiThemeProvider>
+        <div className={classes.links}>
+          <MuiThemeProvider theme={theme}>
+            <Link exact to="/" transition="glide-left">
+              <Button>Accueil</Button>
+            </Link>
+            <Link exact to="/surmesure" transition="glide-left">
+              <Button>Sur-Mesure</Button>
+            </Link>
+            <Link exact to="/tarifs" transition="glide-left">
+              <Button>Délais & Tarifs</Button>
+            </Link>
+            <Link exact to="/rendezvous" transition="glide-left">
+              <Button>Rendez-vous</Button>
+            </Link>
+          </MuiThemeProvider>
+        </div>
       </div>
     </>
   );
--- src/screens/SurMesurePage.js
+++ src/screens/SurMesurePage.js
@@ -2,14 +2,43 @@ import React from 'react';
 import { makeStyles, createTheme } from '@material-ui/core/styles';
 import { Screen } from 'react-tiger-transition';
 import { Grow } from '@material-ui/core';
+import modeleBg from '../images/leon-ell-f6HbVnGtNnY-unsplash.jpg';
 
 const theme = createTheme();
 
 const useStyles = makeStyles({
   root: {
-    '& > *': {
-      margin: 10,
-    },
+    margin: 10,
+    width: '100vw',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  main: {
+    width: '100vw',
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  imageTop: {
+    border: '1px solid red',
+    overflow: 'hidden',
+    width: '100vw',
+    height: '55vh',
+    position: 'relative',
+    top: 0,
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  image: {
+    border: '1px solid blue',
+    width: 1920,
+    height: 2879,
+    position: 'absolute',
+    top: '-120em',
   },
 });
 
@@ -18,7 +47,19 @@ export default function SurMesurePage() {
 
   return (
     <>
-      <div>text</div>
+      <div className={classes.main}>
+        <div className={classes.imageTop}>
+          <img className={classes.image} src={modeleBg} alt="modele-bg" />
+        </div>
+        <div className="select">
+          <select name="location" id="location" value="">
+            Types
+            <option key="robes">Robes</option>
+            <option key="jupes">Jupes</option>
+            <option key="vestes">Vestes</option>
+          </select>
+        </div>
+      </div>
     </>
   );
 }
